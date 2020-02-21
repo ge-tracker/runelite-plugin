8b9bae8db278677a273be0197aaa9b5f40afe50b
James Austen <aus.james@gmail.com>
2020-02-21 12:08:25 +0000

    Updated base files

--- src/main/java/com/getracker/GeTrackerIntegrationPlugin.java
+++ src/main/java/com/getracker/GeTrackerIntegrationPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.getracker;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "GE-Tracker.com Integration"
 )
-public class ExamplePlugin extends Plugin
+public class GeTrackerIntegrationPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private GeTrackerIntegrationConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	GeTrackerIntegrationConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(GeTrackerIntegrationConfig.class);
 	}
 }
